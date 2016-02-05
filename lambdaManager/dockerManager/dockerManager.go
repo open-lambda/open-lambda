84fa10ec6475deae5c50816ff71f4cc674841217
Scott Hendrickson <shendrickso2@wisc.edu>
2016-02-04 18:19:59 -0600

    remove unnecesary docker info call

--- lambdaManager/dockerManager/dockerManager.go
+++ lambdaManager/dockerManager/dockerManager.go
@@ -23,65 +23,61 @@ func RunImg(img string, args []string) (stdout string, stderr string, err error)
 		outBuf bytes.Buffer
 		errBuf bytes.Buffer
 	)
+
 	if client == nil {
 		initClient()
-	} else {
-		// Ensure we are actually connected...
-		if _, err := client.Info(); err != nil {
-			log.Println("failed to get info: %v", err)
-		} else {
-			// Create a new container, with correct args
-			container, err := client.CreateContainer(docker.CreateContainerOptions{
-				Config: &docker.Config{
-					Cmd:          args,
-					AttachStdout: true,
-					AttachStderr: true,
-					Image:        img,
-				},
-				HostConfig: &docker.HostConfig{},
-			})
-			// TODO: This case should attempt to pull container
-			if err != nil {
-				log.Println("failed to create container", err)
-				return "", "", err
-			}
+	}
 
-			// Then run it
-			err = client.StartContainer(container.ID, container.HostConfig)
-			if err != nil {
-				log.Println("failed to start container")
-				return "", "", err
-			}
+	// Create a new container, with correct args
+	container, err := client.CreateContainer(docker.CreateContainerOptions{
+		Config: &docker.Config{
+			Cmd:          args,
+			AttachStdout: true,
+			AttachStderr: true,
+			Image:        img,
+		},
+		HostConfig: &docker.HostConfig{},
+	})
+	// TODO: This case should attempt to pull container
+	if err != nil {
+		log.Println("failed to create container", err)
+		return "", "", err
+	}
 
-			// Wait for container to finish
-			code, err := client.WaitContainer(container.ID)
-			if err != nil {
-				log.Println("wait failed: ", code, err)
-				return "", "", err
-			}
+	// Then run it
+	err = client.StartContainer(container.ID, container.HostConfig)
+	if err != nil {
+		log.Println("failed to start container")
+		return "", "", err
+	}
 
-			// Fill buffers with logs
-			err = client.AttachToContainer(docker.AttachToContainerOptions{
-				Container:    container.ID,
-				OutputStream: &outBuf,
-				ErrorStream:  &errBuf,
-				Logs:         true,
-				Stdout:       true,
-				Stderr:       true,
-			})
-			if err != nil {
-				log.Fatal("failed to attach to container\n", err)
-			}
+	// Wait for container to finish
+	code, err := client.WaitContainer(container.ID)
+	if err != nil {
+		log.Println("wait failed: ", code, err)
+		return "", "", err
+	}
 
-			// remove container
-			err = client.RemoveContainer(docker.RemoveContainerOptions{
-				ID: container.ID,
-			})
-			if err != nil {
-				log.Println("failed to rm container")
-				return "", "", nil
-			}
-		}
+	// Fill buffers with logs
+	err = client.AttachToContainer(docker.AttachToContainerOptions{
+		Container:    container.ID,
+		OutputStream: &outBuf,
+		ErrorStream:  &errBuf,
+		Logs:         true,
+		Stdout:       true,
+		Stderr:       true,
+	})
+	if err != nil {
+		log.Fatal("failed to attach to container\n", err)
+	}
+
+	// remove container
+	err = client.RemoveContainer(docker.RemoveContainerOptions{
+		ID: container.ID,
+	})
+	if err != nil {
+		log.Println("failed to rm container")
+		return "", "", nil
 	}
 
 	return outBuf.String(), errBuf.String(), nil
