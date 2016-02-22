8d189e0a18fb8b2b780475faa4f261e677d7c43f
Scott Hendrickson <shendrickso2@wisc.edu>
2016-02-21 19:43:29 -0600

    pausable framework setup. Pausing comes next.

---
+++ lambdaManager/experimental/docker-pause/dockerManager.go
@@ -0,0 +1,227 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+var client *docker.Client
+
+var registryName string
+
+func init() {
+	// TODO: This requires that users haev pre-configured the environement a docker daemon
+	if c, err := docker.NewClientFromEnv(); err != nil {
+		log.Fatal("failed to get docker client: ", err)
+	} else {
+		client = c
+	}
+	log.Println("docker client initialized")
+}
+
+func SetRegistry(host string, port string) {
+	registryName = fmt.Sprintf("%s:%s", host, port)
+}
+
+func pullContainer(img string) error {
+	err := client.PullImage(
+		docker.PullImageOptions{
+			Repository: img,
+			Registry:   registryName,
+		},
+		docker.AuthConfiguration{})
+	log.Printf("pull of %s complete\n", img)
+
+	if err != nil {
+		log.Printf("failed to pull container: %v\n", err)
+	}
+	return err
+}
+
+func createContainer(img string, args []string) (*docker.Container, error) {
+	// Create a new container with img and args
+	// Specifically give container name of img, so we can lookup later
+
+	// A note on ports
+	// lambdas ALWAYS use port 8080 internally
+	// they are given a random port externally
+	// the client will later lookup the host port by
+	// finding which host port, for a specific
+	// container is bound to 8080
+	// TODO: randomize ports
+	internalAppPort := map[docker.Port]struct{}{"8080/tcp": {}}
+	portBindings := map[docker.Port][]docker.PortBinding{
+		"8080/tcp": {{HostIP: "0.0.0.0", HostPort: "8080"}}}
+	container, err := client.CreateContainer(
+		docker.CreateContainerOptions{
+			Config: &docker.Config{
+				Cmd:          args,
+				AttachStdout: true,
+				AttachStderr: true,
+				Image:        img,
+				ExposedPorts: internalAppPort,
+			},
+			HostConfig: &docker.HostConfig{
+				PortBindings:    portBindings,
+				PublishAllPorts: true,
+			},
+			Name: img,
+		},
+	)
+
+	if err != nil {
+		log.Printf("container %s failed to create with err: %v\n", img, err)
+	}
+	return container, err
+}
+
+func removeContainer(container *docker.Container) error {
+	beforeRemoval := time.Now()
+	// remove container
+	err := client.RemoveContainer(docker.RemoveContainerOptions{
+		ID: container.ID,
+	})
+	afterRemoval := time.Now()
+	log.Printf("container removal took %v\n", afterRemoval.Sub(beforeRemoval))
+	if err != nil {
+		log.Println("failed to rm container")
+		return err
+	}
+
+	return nil
+}
+
+func pullAndCreate(img string, args []string) (container *docker.Container, err error) {
+	container, err = createContainer(img, args)
+	if err != nil {
+		// if the container already exists, don't pull
+		// let client decide how to handle
+		if strings.Contains(err.Error(), "already exists") {
+			return nil, err
+		}
+
+		err = pullContainer(img)
+		if err != nil {
+			log.Printf("container creation failed with: %v\n", err)
+			log.Printf("img pull failed with: %v\n", err)
+			return nil, err
+		} else {
+			container, err = createContainer(img, args)
+			if err != nil {
+				log.Printf("failed to create container %s after good pull, with error: %v\n", img, err)
+				return nil, err
+			}
+		}
+	}
+	return container, nil
+}
+
+func DockerRunImg(img string, args []string) (stdout string, stderr string, err error) {
+	var (
+		outBuf bytes.Buffer
+		errBuf bytes.Buffer
+	)
+
+	// create container
+	container, err := pullAndCreate(img, args)
+	if err != nil {
+		return "", "", err
+	}
+
+	// Then run it
+	err = client.StartContainer(container.ID, container.HostConfig)
+	if err != nil {
+		log.Println("failed to start container")
+		return "", "", err
+	}
+
+	// Wait for container to finish
+	code, err := client.WaitContainer(container.ID)
+	if err != nil {
+		log.Println("wait failed: ", code, err)
+		return "", "", err
+	}
+
+	// Fill buffers with logs
+	err = client.AttachToContainer(docker.AttachToContainerOptions{
+		Container:    container.ID,
+		OutputStream: &outBuf,
+		ErrorStream:  &errBuf,
+		Logs:         true,
+		Stdout:       true,
+		Stderr:       true,
+	})
+
+	if err != nil {
+		log.Fatal("failed to attach to container\n", err)
+	}
+
+	err = removeContainer(container)
+
+	return outBuf.String(), errBuf.String(), nil
+}
+
+func getContainerAddress(container *docker.Container) (address string, err error) {
+	// This gives us: "https://<daemon_ip>:<docker_port>"
+	host := client.Endpoint()
+	// We want: "<daemon_ip>:<lambda_port>"
+
+	// trim bad protocol
+	host = strings.TrimPrefix(host, "https://")
+	// trim old port, keeping ":"
+	idx := strings.Index(host, ":")
+	host = host[:idx+1]
+
+	var buf bytes.Buffer
+	buf.WriteString(host)
+	// add new port
+	for p1, _ := range container.Config.ExposedPorts {
+		buf.WriteString(p1.Port())
+		break
+	}
+	return buf.String(), nil
+}
+
+// returns the running container host in "<host>:<port>" form
+func DockerMakeReady(img string) (containerHost string, err error) {
+	// TODO: decide on one default lambda entry path
+	container, err := pullAndCreate(img, []string{"/go/bin/app"})
+	if err != nil {
+		if strings.Contains(err.Error(), "container already exists") {
+			// make sure container is up
+			// pullAndCraete always sets cId to img
+			containerId := img
+			container, err = client.InspectContainer(containerId)
+			if err != nil {
+				log.Printf("failed to inspect %s with err %v\n", containerId, err)
+				return "", err
+			}
+			if container.State.Paused {
+				// unpause
+				err = client.UnpauseContainer(container.ID)
+				if err != nil {
+					log.Printf("failed to unpause container %s with err %v\n", container.ID, err)
+					return "", err
+				}
+			} else if container.State.Running {
+				// Good to go
+				return getContainerAddress(container)
+			}
+		} else {
+			return "", err
+		}
+	}
+
+	err = client.StartContainer(container.ID, container.HostConfig)
+	if err != nil {
+		log.Println("failed to start container")
+		return "", err
+	}
+
+	return getContainerAddress(container)
+}
---
+++ lambdaManager/experimental/docker-pause/server.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"strings"
+)
+
+// Parses request URL into its "/" delimated components
+// Inspired by http://learntogoogleit.com/post/56844473263/url-path-to-array-in-golang
+func getUrlComponents(r *http.Request) []string {
+	path := r.URL.Path
+
+	// trim beginning
+	if strings.HasPrefix(path, "/") {
+		path = path[1:]
+	}
+
+	// trim trailing "/"
+	if strings.HasSuffix(path, "/") {
+		path = path[:len(path)-1]
+	}
+
+	components := strings.Split(path, "/")
+	return components
+}
+
+func RunLambda(w http.ResponseWriter, r *http.Request) {
+	urlParts := getUrlComponents(r)
+	if len(urlParts) < 2 {
+		http.Error(w, "Name of container image to run required", http.StatusBadRequest)
+		return
+	}
+
+	// components represent runLambda[0]/<name_of_container>[1]/<extra_things>...
+	// ergo we want [1] for name of container
+	img := urlParts[1]
+	log.Printf("running lambda img \"%s\"\n", img)
+
+	// we'll ask docker manager to ensure the img is ready to accept requests
+	// This will either start the img, or unpause a started one
+	// Once this returns, the container is running on "host" ready for connections
+	host, err := DockerMakeReady(img)
+	if err != nil {
+		http.Error(w, "Failed to startup desired lambda", http.StatusInternalServerError)
+		return
+	}
+
+	var urlBuff bytes.Buffer
+	urlBuff.WriteString(host)
+	for _, part := range urlParts {
+		urlBuff.WriteString("/")
+		urlBuff.WriteString(part)
+	}
+	lambdaUrlString := urlBuff.String()
+	log.Printf("proxying request to %s\n", lambdaUrlString)
+	if err != nil {
+		http.Error(w, "failed to create new lambda URL\n", http.StatusInternalServerError)
+		return
+	}
+
+	director := func(req *http.Request) {
+		req = r
+		req.URL.Scheme = "http"
+		req.URL.Host = lambdaUrlString
+	}
+	proxy := &httputil.ReverseProxy{Director: director}
+	proxy.ServeHTTP(w, r)
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatalf("usage: %s <registry hostname> <registry port>\n", os.Args[0])
+	}
+
+	SetRegistry(os.Args[1], os.Args[2])
+
+	http.HandleFunc("/runLambda/", RunLambda)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
