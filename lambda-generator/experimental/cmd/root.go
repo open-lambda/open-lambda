f52bac82e02036cea2fc77a2db7cdbeac29124cc
Scott Hendrickson <shendrickso2@wisc.edu>
2016-03-27 19:09:29 -0500

    Experimental Lambda Generator: sane root descriptions

--- lambda-generator/experimental/cmd/root.go
+++ lambda-generator/experimental/cmd/root.go
@@ -33,14 +33,9 @@ var (
 
 // This represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "experimental",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   fmt.Sprintf("%s", os.Args[0]),
+	Short: "Helps to template, organize, build and deploy OpenLambda Lambdas",
+	Long:  "Helps to template, organize, build and deploy OpenLambda Lambdas",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
@@ -74,7 +69,6 @@ func init() {
 		fmt.Printf("WARNING: no .openlambda directory found (Have you called %s init yet?)\n\n", os.Args[0])
 		return
 	}
-	fmt.Printf("using .openlambda at %s\n", olDir)
 
 	// Here we select the frontend, based on user configs found from above
 	switch frontendStr {
