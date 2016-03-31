3b8286801ae68f9976853dd827c90ef6c64fe9c8
Scott Hendrickson <shendrickso2@wisc.edu>
2016-03-31 08:46:43 -0500

    limit frontend interface to required impls

--- lambda-generator/experimental/frontends/frontends.go
+++ lambda-generator/experimental/frontends/frontends.go
@@ -1,17 +1,18 @@
 package frontends
 
 type FrontEnd interface {
-	// returns the human-readable name of this frontend
-	FrontEndName() string
 
 	// Adds a template lambda at <location>
+	// Must be provided by frontend
 	AddLambda(location string)
 
 	// Builds lambda at <path>
+	// Must be provided by frontend
 	BuildLambda(path string)
 
 	// Returns the ID for a given lambda path
 	// Id's are used to identify an individual lambda throughout the system
 	// Docker image tags are an example use of Id's
+	// Implemented in base.go, override only if needed
 	GetId(path string) (string, error)
 }
