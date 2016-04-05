0e8105dc5aa722a4425d9f114a3c0ceaa713a2a4
Tyler Harter <tylerharter@gmail.com>
2016-04-05 14:11:39 -0400

    use multi-process flask for python wrappers

--- lambda-generator/pyserver/server.py
+++ lambda-generator/pyserver/server.py
@@ -6,6 +6,8 @@ import cgi
 import traceback, json, time, os, socket, struct
 import lambda_func # assume submitted .py file is called lambda_func
 import rethinkdb
+import flask
+app = flask.Flask(__name__)
 
 PORT = 8080
 
@@ -20,46 +22,34 @@ def get_default_gateway_linux():
 
             return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
 
-class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kvargs):
-        # gateway will refer to the Docker host on which this container runs
-        self.db_conn = rethinkdb.connect(get_default_gateway_linux(), 28015)
+db_conn_cache = None
 
-        # do_POST is called by SimpleHTTPRequestHandler.__init__, so
-        # do this after any other init
-        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, *args, **kvargs)
+def db_conn():
+    global db_conn_cache
+    if db_conn_cache == None:
+        db_conn_cache = rethinkdb.connect(get_default_gateway_linux(), 28015)
+    return db_conn_cache
 
-    def send_result(self, result):
-        self.send_response(200) # OK
-        self.send_header('Content-type', 'text/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(result))
-
-    def do_GET(self):
-        pass
-
-    def do_POST(self):
-        length = int(self.headers.getheader('content-length'))
-        # parse req
-        data = self.rfile.read(length)
-        try :
-            event = json.loads(data)
-        except:
-            self.send_result('could not parse ' + str(data))
-            return
-        # handle req
-        try :
-            result = lambda_func.handler(self.db_conn, event)
-        except Exception:
-            self.send_result(traceback.format_exc())
-            return
-        # respond
-        self.send_result(result)
+# catch everything
+@app.route('/', defaults={'path': ''}, methods=['POST'])
+@app.route('/<path:path>', methods=['POST'])
+def flask_post(path):
+    flask.request.get_data()
+    data = flask.request.data
+    try:
+        event = json.loads(data)
+    except:
+        return 'could not parse ' + str(data)
+    # handle req
+    try:
+        return json.dumps(lambda_func.handler(db_conn(), event))
+    except Exception:
+        return traceback.format_exc()
 
 def main():
-    httpd = SocketServer.TCPServer(("", PORT), ServerHandler)
-    print "serving at port", PORT
-    httpd.serve_forever()
+    # TODO(tyler): shouldn't be fixed at 10 (make dynamic, or maybe
+    # use config)
+    app.run(processes=10, host='0.0.0.0', port=PORT)
 
 if __name__ == '__main__':
     main()
