d7aa44f33ecaff080b50e85ea413ea84b8e70977
Tyler Harter <tylerharter@gmail.com>
2016-04-06 16:08:16 -0400

    print directions for non-local tests in startup script

--- util/start-local-cluster.py
+++ util/start-local-cluster.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os, sys, subprocess, json, argparse, time
+import netifaces
 import rethinkdb as r
 from common import *
 
@@ -7,11 +8,16 @@ def container_ip(cid):
     inspect = run_js('docker inspect '+cid)
     return only(inspect)['NetworkSettings']['IPAddress']
 
-def lookup_registry_port(cid):
+def lookup_host_port(cid, guest_port):
     inspect = run_js('docker inspect '+cid)
-    return only(only(inspect)['NetworkSettings']['Ports']['5000/tcp'])['HostPort']
+    return only(only(inspect)['NetworkSettings']['Ports'][str(guest_port)+'/tcp'])['HostPort']
+
+def my_ip(name='eth0'):
+    ip = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
+    return ip
 
 def main():
+    host_ip = my_ip()
     parser = argparse.ArgumentParser(description='number of workers')
     parser.add_argument('--workers', '-w', default='1')
     args = parser.parse_args()
@@ -23,14 +29,16 @@ def main():
     os.mkdir(cluster_dir)
 
     # start registry
-    c = 'docker run -d -p 5000 registry:2'
+    c = 'docker run -d -p 0:5000 registry:2'
     cid = run(c).strip()
     registry_ip = container_ip(cid)
-    registry_port = lookup_registry_port(cid)
+    registry_port = lookup_host_port(cid, 5000)
     config_path = os.path.join(cluster_dir, 'registry.json')
     config = {'cid': cid,
               'ip': registry_ip,
+              'host_ip': host_ip,
               'host_port': registry_port}
+    print config
     wrjs(config_path, config)
     print 'started registry ' + registry_ip + ':5000 (or localhost:' + registry_port + ')'
     print '='*40
@@ -54,6 +62,8 @@ def main():
         cid = run(c).strip()
         config['cid'] = cid
         config['ip'] = container_ip(cid)
+        config['host_ip'] = host_ip
+        config['host_port'] = lookup_host_port(cid, 8080)
         wrjs(config_path, config, atomic=True)
 
         info_path = os.path.join(cluster_dir, 'worker-info-%d.json' % i)
@@ -77,9 +87,14 @@ def main():
     print '='*40
     print 'Push images to OpenLambda registry as follows (or similar):'
     print 'IMG=hello && docker tag $IMG localhost:%s/$IMG; docker push localhost:%s/$IMG' % (registry_port, registry_port)
+    print 'OR'
+    print ('IMG=hello && docker tag $IMG %s:%s/$IMG; docker push %s:%s/$IMG' %
+           (host_ip, registry_port, host_ip, registry_port))
     print '='*40
     print 'Send requests as follows (or similar):'
     print "IMG=hello && curl -X POST %s:8080/runLambda/$IMG -d '{}'" % workers[-1]['ip']
+    print 'OR'
+    print "IMG=hello && curl -X POST %s:%s/runLambda/$IMG -d '{}'" % (host_ip, workers[-1]['host_port'])
 
 if __name__ == '__main__':
     main()
