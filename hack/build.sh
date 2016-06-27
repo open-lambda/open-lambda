f3afb992f9913307f2ebba7cecc106edda39c312
Tyler Harter <tylerharter@gmail.com>
2016-06-27 10:27:26 -0400

    make go path hack more visible

--- hack/build.sh
+++ hack/build.sh
@@ -7,7 +7,7 @@
 set -e
 
 export WD=$(pwd)
-export GOPATH=${WD}'/.gopath'
+export GOPATH=${WD}'/go'
 export CODE_BASE='../worker'
 export LAMBDA_PACKAGE=${GOPATH}'/src/github.com/open-lambda/open-lambda'
 export WORKER=${LAMBDA_PACKAGE}'/worker/'
@@ -16,15 +16,13 @@ export CLIENT=${LAMBDA_PACKAGE}'/worker/prof/client'
 # setup commit hooks
 ln -s ${WD}/pre-commit ${WD}/../.git/hooks/ >/dev/null 2>/dev/null || true
 
-# init gopath
-mkdir -p ${LAMBDA_PACKAGE}
-ln -sf ${WD}/${CODE_BASE} ${LAMBDA_PACKAGE}
-
 # build
 if [[ $1 == "test" ]]
 then
+    set -e -x
     cd ${WORKER}; go test ${@:2}
 else
+    set -e -x
     # just compile
     cd ${WORKER}; go get; go build
     cd ${CLIENT}; go get; go build
