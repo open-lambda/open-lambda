6c0992ddf9b42d94b5bbb3b31d05b4440e9db063
Ed Oakes <eoakes@wisc.edu>
2016-09-06 12:26:42 -0400

    removed newline

--- applications/hello/lambda_func.py
+++ applications/hello/lambda_func.py
@@ -1,5 +1,5 @@
 def handler(conn, event):
     try:
-        return "Hello, %s!\n" % event['name']
+        return "Hello, %s!" % event['name']
     except Exception as e:
         return {'error': str(e)} 
