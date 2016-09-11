c9c3ac8d69680e16a3ea61278c302dfad9bddfaf
Leon Yang <leon.gh.yang@gmail.com>
2016-09-11 00:02:34 +0000

    Add dependency on ubuntu support for AUFS
    See https://docs.docker.com/engine/installation/linux/ubuntulinux/#/prerequisites-by-ubuntu-version

--- quickstart/deps.sh
+++ quickstart/deps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 add-apt-repository ppa:ubuntu-lxc/lxd-stable -y
 apt-get update
+apt-get -y install linux-image-extra-$(uname -r)
+apt-get -y install linux-image-extra-virtual
 apt-get -y install golang
 apt-get -y install docker.io
 apt-get -y install docker-engine
