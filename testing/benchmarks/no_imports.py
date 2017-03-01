5f5b1f880c72dac18d06bcbe32bed5a0c6c56ea5
Kevin Houck <kevin.houck.95@gmail.com>
2017-02-28 22:38:15 -0600

    Add min/max/avg to test script, fix any cleanup/setup race conditions

--- testing/benchmarks/no_imports.py
+++ testing/benchmarks/no_imports.py
@@ -52,15 +52,15 @@ def clean_for_test(cluster_name):
     except Exception:
         pass
     try:
+        run('sudo rm -rf ' + cluster_name, quiet=True)
+    except Exception:
+        pass
+    try:
         run('sudo docker unpause $(docker ps -a -q)', quiet=True)
     except Exception:
         pass
     try:
         run('sudo docker kill $(docker ps -a -q)', quiet=True)
-    except Exception:
-        pass
-    try:
-        run('sudo rm -rf ' + cluster_name, quiet=True)
     except Exception:
         pass
 
@@ -84,22 +84,32 @@ def interpreters_no_containers():
 def benchmark(stage, type, which_lambda, iterations):
     if stage == 'WARM':
         clean_for_test(type)
+        sleep(1)
         setup_cluster(type)
-        sleep(0.10)
+        sleep(1)
         run_lambda(which_lambda)
 
+    min = 100000
+    max = -1
     total_time = 0
     for i in range(0, iterations):
         if stage == 'COLD':
             clean_for_test(type)
+            sleep(1)
             setup_cluster(type)
-            sleep(0.10)
+            sleep(1)
         before = get_time_millis()
         run_lambda(which_lambda)
         after = get_time_millis()
-        total_time += after - before
-    #clean_for_test(type)
-    return int(round(total_time / iterations))
+        iter_time = after - before
+        total_time += iter_time
+        if iter_time < min:
+            min = iter_time
+        if iter_time > max:
+            max = iter_time
+    clean_for_test(type)
+    avg = int(round(total_time / iterations))
+    return {'min': min, 'max': max, 'avg': avg}
 
 # do no change these unless you also change config file cluster_name and their file names
 NO_INTERPRETERS_NO_CONTAINERS = 'ninc'
@@ -111,18 +121,22 @@ ITERATIONS = 5
 
 # No container pool and no interpreter pool
 # warm, worker will have code pulled already
-avg = benchmark('WARM', NO_INTERPRETERS_NO_CONTAINERS, 'hello', ITERATIONS)
-print 'No container pool, no interpreter pool, code already pulled: ' + str(avg) + ' ms avg'
+res = benchmark('WARM', NO_INTERPRETERS_NO_CONTAINERS, 'hello', ITERATIONS)
+print 'No container pool, no interpreter pool, code already pulled (min/max/avg ms): ' + \
+      str(res['min']) + ' ' + str(res['max']) + ' ' + str(res['avg'])
 
 # cold, worker will have to pull handler
-avg = benchmark('COLD', NO_INTERPRETERS_NO_CONTAINERS, 'hello', ITERATIONS)
-print 'No container pool, no interpreter pool, code not already pulled: ' + str(avg) + ' ms avg'
+res = benchmark('COLD', NO_INTERPRETERS_NO_CONTAINERS, 'hello', ITERATIONS)
+print 'No container pool, no interpreter pool, code not already pulled (min/max/avg ms): ' + \
+      str(res['min']) + ' ' + str(res['max']) + ' ' + str(res['avg'])
 
 # No container pool and interpreter pool
 # warm, worker will have code pulled already
-avg = benchmark('WARM', INTERPRETERS_NO_CONTAINERS, 'hello', ITERATIONS)
-print 'No container pool, interpreter pool, code already pulled: ' + str(avg) + ' ms avg'
+res = benchmark('WARM', INTERPRETERS_NO_CONTAINERS, 'hello', ITERATIONS)
+print 'No container pool, interpreter pool, code already pulled (min/max/avg ms): ' +  \
+      str(res['min']) + ' ' + str(res['max']) + ' ' + str(res['avg'])
 
 # cold, worker will have to pull handler
-avg = benchmark('COLD', INTERPRETERS_NO_CONTAINERS, 'hello', 5)
-print 'No container pool, interpreter pool, code not already pulled: ' + str(avg) + ' ms avg'
+res = benchmark('COLD', INTERPRETERS_NO_CONTAINERS, 'hello', 5)
+print 'No container pool, interpreter pool, code not already pulled (min/max/avg ms): ' + \
+      str(res['min']) + ' ' + str(res['max']) + ' ' + str(res['avg'])
