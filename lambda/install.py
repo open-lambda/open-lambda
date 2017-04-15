a9af0e747340c97fc51b2daab2e47c0005976d26
Kevin Houck <kevin.houck.95@gmail.com>
2017-04-15 17:31:16 -0500

    Change pip install for local mirror

--- lambda/install.py
+++ lambda/install.py
@@ -20,7 +20,7 @@ def main():
             try:
                 pkg = line.split(':')[1]
                 if pkg != '':
-                    check_output(['pip', 'install', pkg])
+                    check_output(['pip', 'install', '--index-url', 'http://192.168.103.144:9199/simple', '--trusted-host', '192.168.103.144', pkg])
                     install(pkg)
             except Exception as e:
                 print('failed to install %s with %s' % (pkg, e))
