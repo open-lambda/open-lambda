b8dab5823189d51c6f5d45673ff4bba77a921de9
Kevin Houck <kevin.houck.95@gmail.com>
2017-04-15 13:22:41 -0500

    Alow exact values and exact distributions

--- pipbench/helper_modules/distribution.py
+++ pipbench/helper_modules/distribution.py
@@ -8,8 +8,17 @@ class Distribution:
         self.dist_args = dist_args
 
     def sample(self):
-        dist = getattr(numpy.random, self.dist)
-        return abs(math.ceil(dist(**self.dist_args)))
+        if self.dist == 'exact_distribution':
+            r = numpy.random.randint(0, 1)
+            total = 0
+            for v in self.dist_args['values']:
+                if v['weight'] + total > r and total < r:
+                     return int(v['value'])
+        elif self.dist == 'exact_value':
+            return self.dist_args['value']
+        else:
+            dist = getattr(numpy.random, self.dist)
+            return abs(math.ceil(dist(**self.dist_args)))
 
 
 def distribution_factory(dist_spec):
