954e042160d00a1cf034495841a368eac61029da
Kevin Houck <kevin.houck.95@gmail.com>
2017-04-18 17:41:15 -0600

    Add latency, 500 tolerance, clean exiting

--- testing/benchmarks/run_workload.py
+++ testing/benchmarks/run_workload.py
@@ -12,23 +12,51 @@ import grequests
 import requests
 import multiprocessing
 import datetime
+import statistics
 
 TRACE_RUN = False
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class Distribution:
-    def __init__(self, dist, dist_args):
+    def __init__(self, dist, transform, dist_args):
         self.dist = dist
         self.dist_args = dist_args
+        self.transform = transform
 
     def sample(self):
-        dist = getattr(numpy.random, self.dist)
-        return abs(math.ceil(dist(**self.dist_args)))
+        val = None
+        if self.dist == 'exact_distribution':
+            r = numpy.random.randint(0, 1)
+            total = 0
+            for v in self.dist_args['values']:
+                if total < r and total < r + v['weight']:
+                    val = v['value']
+                    break
+                total += v['weight']
+        elif self.dist == 'exact_distribution_uniform':
+            i = numpy.random.randint(0, 20)
+            val = self.dist_args['values'][i]
+        elif self.dist == 'exact_value':
+            val = self.dist_args['value']
+        else:
+            dist = getattr(numpy.random, self.dist)
+            val = dist(**self.dist_args)
+
+        if self.transform:
+            if self.transform == 'float_s_to_int_ms':
+                return round(val * 1000)
+        else:
+            return abs(math.ceil(val))
+
 
 def distribution_factory(dist_spec):
     dist = dist_spec['dist']
     dist_spec.pop('dist')
-    return Distribution(dist, dist_spec)
+    transform = None
+    if 'transform' in dist_spec:
+        transform = dist_spec['transform']
+        dist_spec.pop('transform')
+    return Distribution(dist, transform, dist_spec)
 
 def run(cmd, quiet=False):
     if TRACE_RUN:
@@ -62,8 +90,8 @@ def make_blocking_request(handler_name):
     }))
     end = get_time_millis()
     now = datetime.datetime.now()
-    result_str = '[%s] handler: %s status: %d in: %d ms' % ( datetime.datetime.isoformat(now), handler_name, r.status_code, end-start)
-    return result_str
+    result = {"time": datetime.datetime.now(), "handler_name": handler_name, "status_code": r.status_code, "latency": end-start}
+    return result
 
 def fork_and_make_request(handler_name):
     p = multiprocessing.Process(target=make_blocking_request, args=(handler_name,))
@@ -75,7 +103,7 @@ def get_time_millis():
 
 def parse_config(config_file_name, handler_dir):
     if config_file_name is None:
-        return {
+        config = {
             "cycles": 10, # amount
             "cycleInterval": 5, # time between cycle starts in ms
             "handlerGroups": [
@@ -91,83 +119,154 @@ def parse_config(config_file_name, handler_dir):
                         "loc": 10.0,
                         "scale": 5.0
                     },
-                    # handlerRegex
-                    "handlers": [
-
-                    ]
-                }
-            ]
+                    "handlerRegex": ".*"
+                },
+            ],
+            "workload": {
+                "type": "latency",
+                "latencyDist": {
+                    "dist": "exact_value",
+                    "value": 1000 # ms
+                },
+                "500_tolerance": 1 # a percentile, out of int 100
+            }
         }
     else:
         f = open(config_file_name, 'r')
         config = json.load(f)
-        # if handlerRegex present populate handlers[] from it
-        for handler_group in config["handlerGroups"]:
-            # construct distributions
-            handler_group["runSample"] = distribution_factory(handler_group["runSample"])
-            handler_group["runAmount"] = distribution_factory(handler_group["runAmount"])
-            # find handlers that belong to group if necessary
-            if "handlerRegex" in handler_group:
-                matched_handlers = []
-                ro = re.compile(handler_group["handlerRegex"])
-                present_handlers = os.listdir('%s/%s' % (SCRIPT_DIR, handler_dir))
-                for handler_name in present_handlers:
-                    if ro.match(handler_name):
-                        matched_handlers.append(handler_name)
-                handler_group["handlers"] = matched_handlers
+    if config["workload"]["type"] == 'latency':
+        config["workload"]["latencyDist"] = distribution_factory(config["workload"]["latencyDist"])
+    # if handlerRegex present populate handlers[] from it
+    for hg in (config["handlerGroups"]):
+        # construct distributions
+        hg["runSample"] = distribution_factory(hg["runSample"])
+        hg["runAmount"] = distribution_factory(hg["runAmount"])
+        # find handlers that belong to group if necessary
+        if "handlerRegex" in hg:
+            matched_handlers = []
+            ro = re.compile(hg["handlerRegex"])
+            present_handlers = os.listdir('%s/%s' % (SCRIPT_DIR, handler_dir))
+            for handler_name in present_handlers:
+                if ro.match(handler_name):
+                    matched_handlers.append(handler_name)
+            hg["handlers"] = matched_handlers
 
-        return config
+    return config
 
 def cleanup_children(children):
     for k in range(0, len(children)):
         children[k].join()
 
-def request_runner(config, id, log_queue):
+def request_runner(config, id, log_queue, shared_bool):
     print('Request runner %d started' % id)
-    while True:
+    while shared_bool.value != 1:
         for handler_group in config["handlerGroups"]:
             if numpy.random.random() < handler_group["runSample"].sample():
                 num_to_run = handler_group["runAmount"].sample()
                 for j in range(0, num_to_run):
                     handlers = handler_group["handlers"]
                     num_hanlders = len(handlers)
+                    start = time.clock()
                     handler_to_run = handlers[random.randint(0, num_hanlders - 1)]
-                    res_str = make_blocking_request(handler_to_run)
+                    try:
+                        res_str = make_blocking_request(handler_to_run)
+                    except Exception as e:
+                        log_queue.put("lost_connection")
+                        return
                     log_queue.put(res_str)
+                    if config["workload"]["type"] == 'latency':
+                        elapsed_time = time.clock() - start
+                        latency =  config["workload"]["latencyDist"].sample()
+                        if elapsed_time / 1000 < latency:
+                            time.sleep((latency - elapsed_time) / 1000)
+    log_queue.put(None)
 
-
-def log_queue_consumer(stats_queue, log_file_name):
+def log_queue_consumer(config, stats_queue, log_file_name, num_minutes, num_producers, end_on_error):
+    start = time.clock()
+    results = []
     f = None
     if log_file_name:
         f = open(log_file_name, 'w')
-    while True:
+    end_count = 0
+    num_500s = 0
+    while time.clock() - start < num_minutes * 60:
         log_entry = stats_queue.get()
-        print(log_entry)
-        if f:
-            f.write(log_entry + '\n')
+        if log_entry == None:
+            end_count += 1
+            if end_count == num_producers:
+                break
+            continue
+        elif type(log_entry) is str:
+            print("Could not find server")
+            f.write("Could not find server")
+            break
+        else:
+            results.append(log_entry)
+            log_str = '[%s] handler: %s status: %d in: %d ms' % ( datetime.datetime.isoformat(log_entry["time"]), log_entry["handler_name"], log_entry["status_code"], log_entry["latency"])
+            print(log_str)
+            if f:
+                f.write(log_str + '\n')
+            # check if above 500 tolerance
+            if log_entry["status_code"] == 500:
+                num_500s += 1
+                if float(num_500s) / len(results) > config["workload"]["500_tolerance"] / 100:
+                    print("500 Tolerance Reached, stopping benchmark...")
+                    f.write("500 Tolerance Reached, stopping benchmark...")
+                    break
+    # compute handler results
+    handlers = []
+    for l in results:
+        if not l["handler_name"] in handlers:
+            handlers.append(l["handler_name"])
+    for h in handlers:
+        reqs = []
+        for l in results:
+            if l["handler_name"] == h and l["status_code"] == 200:
+                reqs.append(l)
+        r = [req['latency'] for req in reqs]
+        if len(r) != 0:
+            f.write("Hanlder %s avg latency: %d\n" % (h, statistics.mean(r)))
+        f.write("Total num reqs to handler %s: %d\n" % (h, len(r)))
+    # compute total results
+    only_200s = []
+    for l in results:
+        if l["status_code"] == 200:
+            only_200s.append(l)
+
+    if len(only_200s) != 0:
+        f.write("Overall Average latency(200s only): %d\n" % (statistics.mean([ent['latency'] for ent in only_200s])))
+    f.write("Overall throughput(200s only): %d\n" % len(only_200s))
+    f.close()
+    # tell parent we're done
+    end_on_error.value = 1
+
+def get_latency(le):
+    return le["latency"]
 
 def benchmark(config, num_minutes, num_req_runners, log_file_name):
     req_runners = []
     log_queue = multiprocessing.Queue(10000)
+    runner_flag = multiprocessing.Value('b')
+    end_on_error = multiprocessing.Value('b')
+
     print('Creating log queue consumer')
-    queue_consumer = multiprocessing.Process(target=log_queue_consumer, args=(log_queue, log_file_name))
+    queue_consumer = multiprocessing.Process(target=log_queue_consumer, args=(config, log_queue, log_file_name, num_minutes, num_req_runners, end_on_error))
     queue_consumer.start()
 
     print('Creating %d request runners' % num_req_runners)
     for i in range(0, num_req_runners):
-        p = multiprocessing.Process(target=request_runner, args=(config, i, log_queue))
+        p = multiprocessing.Process(target=request_runner, args=(config, i, log_queue, runner_flag))
         p.start()
         req_runners.append(p)
 
     start = time.clock()
     while time.clock() - start < num_minutes * 60:
-        pass
-
+        if end_on_error.value == 1:
+            break
+    runner_flag.value = 1
     for p in req_runners:
-        p.terminate()
         p.join()
 
-    queue_consumer.terminate()
     queue_consumer.join()
 
 
@@ -201,3 +300,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
