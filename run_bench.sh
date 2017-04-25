8dd53bdfd9e450b417b9945ae61cb2abb292f65e
Mitchell Manar <mitchell.manar@gmail.com>
2017-04-24 21:39:41 -0500

    json bench working (also requires gen'd files)

--- run_bench.sh
+++ run_bench.sh
@@ -18,7 +18,17 @@ sleep 3
 #curl -X POST localhost:8080/runLambda/call_perf -d '{}'
 #curl -X POST localhost:8080/runLambda/ipc_test -d '{}'
 #curl -X POST localhost:8080/runLambda/fork_test -d '{}'
+#curl -X POST localhost:8080/runLambda/pickle_test -d '{}'
+#curl -X POST localhost:8080/runLambda/proto_test -d '{}'
 
 # IPC bench
-curl -X POST localhost:8081/runLambda/ipc_resp -d '{}' & # setup resp
-curl -X POST localhost:8080/runLambda/ipc_call -d '{}' # execute call
+#curl -X POST localhost:8081/runLambda/ipc_resp -d '{}' & # setup resp
+#curl -X POST localhost:8080/runLambda/ipc_call -d '{}' # execute call
+
+# Serial test: pickle
+#curl -X POST localhost:8081/runLambda/pkl_resp -d '{"num_keys":"1", "depth" : "1", "value_len" : "1"}' & # setup resp
+#curl -X POST localhost:8080/runLambda/pkl_call -d '{"num_keys":"1", "depth" : "1", "value_len" : "1"}' # execute call
+
+# Serial test: json (still uses pkl_resp)
+curl -X POST localhost:8081/runLambda/pkl_resp -d '{"num_keys":"1", "depth" : "1", "value_len" : "1"}' & # setup resp
+curl -X POST localhost:8080/runLambda/json_call -d '{"num_keys":"1", "depth" : "1", "value_len" : "1"}' # execute call
