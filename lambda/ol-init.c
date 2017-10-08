7c43126692977cb4a5373a5a4f0c60f9d6a3cfc2
Dennis Zhou <dennisszhou@gmail.com>
2017-10-08 14:09:32 -0600

    Use sigaction instead of signal
    Signal is not portable. It is better to use sigaction so we can also
    prevent other signals from interrupting us just in case.

--- lambda/ol-init.c
+++ lambda/ol-init.c
@@ -7,13 +7,30 @@
 
 char **params;
 
-// double fork to avoid zombies and exec the python server
+/*
+ * Launch the lambda server.
+ */
 void signal_handler() {
 	if (fork() == 0) {
 		execv(params[0], params);
 	}
 
 	return;
+}
+
+/*
+ * Install the handler and block all other signals while handling
+ * the signal. Reset the signal handler after caught to default.
+ */
+void install_handler() {
+	struct sigaction setup_action;
+	sigset_t block_mask;
+
+	sigfillset(&block_mask);
+	setup_action.sa_handler = signal_handler;
+	setup_action.sa_mask = block_mask;
+	setup_action.sa_flags = SA_RESETHAND;
+	sigaction(SIGURG, &setup_action, NULL);
 }
 
 int main(int argc, char *argv[]) {
@@ -27,7 +44,7 @@ int main(int argc, char *argv[]) {
 	}
 	params[argc+1] = NULL;
 
-	signal(SIGURG, signal_handler);
+	install_handler();
 	while (1) {
 		pause(); // sleep forever, we're init for the ns
 	}
