73150b863d2d64043357866688149e94dd7b4bd8
Ed Oakes <ed.nmi.oakes@gmail.com>
2018-02-20 04:56:13 +0000

    Remove pipbench code

--- pipbench/dump_sizes.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('new-graph-100000.json') as fd:
-    spec = json.load(fd)
-
-with open('package_sizes.txt', 'w') as fd:
-    for pkg in spec:
-        fd.write('%s:%s\n' % (pkg['name'], pkg['uncompressed']))
--- pipbench/setup.sh
+++
@@ -1,6 +0,0 @@
-pip3 install falcon
-pip3 install gunicorn
-pip3 install pypiserver
-pip3 install requests
-pip3 install numpy
-pip3 install grequests
