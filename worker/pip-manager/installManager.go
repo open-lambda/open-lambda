55c374c155e26bb83b940598cf3d8e32a07630f1
Gustavo <gtotoy@gmail.com>
2018-07-31 15:20:00 -0500

    Use defer to unlock mutexes from Install method of installManager.go

--- worker/pip-manager/installManager.go
+++ worker/pip-manager/installManager.go
@@ -56,26 +56,24 @@ func InitInstallManager(opts *config.Config) (*Installer, error) {
 func (i *Installer) Install(pkgs []string) error {
 	for _, pkg := range pkgs {
 		i.mutex.Lock()
+		defer i.mutex.Unlock()
 		pkgState, ok := i.pkgStates[pkg]
 		if !ok {
 			rwMutex := &sync.RWMutex{}
 			rwMutex.Lock()
+			defer rwMutex.Unlock()
 			i.pkgStates[pkg] = &PackageState{rwMutex}
-			i.mutex.Unlock()
 
 			cmd := exec.Command(i.cmd, append(i.args, pkg)...)
 			if err := cmd.Run(); err != nil {
 				delete(i.pkgStates, pkg)
-				i.mutex.Unlock()
-				return fmt.Errorf("failed to install package '%s' :: %v", pkg, err)
+				return fmt.Errorf("failed to install package '%s' :: error %v :: cmd %v", pkg, err, cmd)
 			}
-			rwMutex.Unlock()
 		} else {
 			// The ordering here will have to change when we implement package
 			// eviction - the package could be evicted after dropping the global
 			// lock. We will also have to release reader locks on eviction of
 			// handlers/cache entries.
-			i.mutex.Unlock()
 			pkgState.mutex.RLock()
 		}
 	}
