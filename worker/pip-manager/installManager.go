5b9f2573c38fc5f5046db35fdbf3d4319189caca
Gustavo <gtotoy@gmail.com>
2018-08-04 12:17:56 -0500

    Add i.mutex.Lock() before delete(i.pkgStates, pkg) to avoid calling i.mutex.Unlock() twice when cmd returns an error.

--- worker/pip-manager/installManager.go
+++ worker/pip-manager/installManager.go
@@ -60,16 +60,16 @@ func (i *Installer) Install(pkgs []string) error {
 		if !ok {
 			rwMutex := &sync.RWMutex{}
 			rwMutex.Lock()
+			defer rwMutex.Unlock()
 			i.pkgStates[pkg] = &PackageState{rwMutex}
 			i.mutex.Unlock()
-
 			cmd := exec.Command(i.cmd, append(i.args, pkg)...)
 			if err := cmd.Run(); err != nil {
+				i.mutex.Lock()
 				delete(i.pkgStates, pkg)
 				i.mutex.Unlock()
-				return fmt.Errorf("failed to install package '%s' :: %v", pkg, err)
+				return fmt.Errorf("failed to install package '%s' :: %v :: %v", pkg, err, cmd)
 			}
-			rwMutex.Unlock()
 		} else {
 			// The ordering here will have to change when we implement package
 			// eviction - the package could be evicted after dropping the global
