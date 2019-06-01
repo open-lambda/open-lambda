bd8d64db262be26f87b411de562d82c74a5d8968
Tyler Caraza-Harter <tylerharter@gmail.com>
2019-06-01 23:43:21 +0000

    move ol dir to src

--- travis.sh
+++ travis.sh
@@ -3,15 +3,15 @@
 unset GOPATH
 unset GOROOT
 
-GO_FILES=$(cd ol && find . -type f ! -path './vendor/*' -name '*.go')
+GO_FILES=$(cd src && find . -type f ! -path './vendor/*' -name '*.go')
 
 # format test
-if [[ $(cd ol && gofmt -d $GO_FILES) ]]; then
+if [[ $(cd src && gofmt -d $GO_FILES) ]]; then
 	cat <<EOF
 Error: format check failed
-Please format "ol" directory with the following command and commit again:
+Please format "src" directory with the following command and commit again:
 
-    gofmt -w -l \$(find $PWD/ol ! -path '*/vendor/*' -name '*.go')
+    gofmt -w -l \$(find $PWD/src ! -path '*/vendor/*' -name '*.go')
 EOF
 	exit 1
 fi
