2b41bbebbd8f6f66303423513e2ae8d5dee756ea
Tyler Caraza-Harter <tylerharter@gmail.com>
2019-06-01 23:36:09 +0000

    add benchmarks to tests

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-import os, sys, json, time, requests, copy, traceback
+import os, sys, json, time, requests, copy, traceback, tempfile
 from collections import OrderedDict
 from subprocess import Popen
+from multiprocessing import Pool
 from contextlib import contextmanager        
                 
 
@@ -13,18 +14,22 @@ curr_conf = None
 
 def test(fn):
     def wrapper(*args, **kwargs):
-        rc = None
+        if len(args):
+            raise Exception("positional args not supported for tests")
+
         result = OrderedDict()
         result["test"] = fn.__name__
+        result["params"] = kwargs
         result["pass"] = None
         result["seconds"] = None
+        result["stats"] = None
         result["conf"] = curr_conf
         result["exception"] = None
         result["worker_tail"] = None
 
         t0 = time.time()
         try:
-            rc = fn(*args, **kwargs)
+            result["stats"] = fn(**kwargs)
             result["pass"] = True
         except Exception:
             result["pass"] = False
@@ -37,7 +42,7 @@ def test(fn):
 
         results["runs"].append(result)
         print(json.dumps(result, indent=2))
-        return rc
+        return result["stats"]
 
     return wrapper
 
@@ -91,7 +96,7 @@ def test_smoke_echo():
 
 
 @test
-def test_smoke_install(num=None):
+def test_smoke_install(num):
     name = "install"
     if num != None:
         name += str(num)
@@ -99,16 +104,64 @@ def test_smoke_install(num=None):
     if r.status_code != 200:
         raise Exception("STATUS %d: %s" % (r.status_code, r.text))
     assert r.json() == "imported"
-    
+
 
 def smoke_tests():
     test_smoke_echo()
-    test_smoke_install()
-    test_smoke_install(2)
-    test_smoke_install(3)
+    test_smoke_install(num=None)
+    test_smoke_install(num=2)
+    test_smoke_install(num=3)
+
+
+def stress_one_lambda_task(args):
+    t0, seconds = args
+    i = 0
+    while time.time() < t0 + seconds:
+        r = requests.post("http://localhost:5000/run/echo", data=str(i))
+        r.raise_for_status()
+        assert r.text == str(i)
+        i += 1
+    return i
+
+
+@test
+def stress_one_lambda(procs, seconds):
+    t0 = time.time()
+
+    with Pool(procs) as p:
+        reqs = sum(p.map(stress_one_lambda_task, [(t0, seconds)] * procs, chunksize=1))
+
+    return {"reqs_per_sec": reqs/seconds}
+
+
+@test
+def call_each_once_exec(lambda_count):
+    # TODO: do in parallel
+    t0 = time.time()
+    for i in range(lambda_count):
+        r = requests.post("http://localhost:5000/run/L%d"%i, data="null")
+        r.raise_for_status()
+        assert r.text == str(i)
+    seconds = time.time() - t0
+
+    return {"reqs_per_sec": lambda_count/seconds}
+
+
+def call_each_once(lambda_count):
+    with tempfile.TemporaryDirectory() as reg_dir:
+        # create dummy lambdas
+        for i in range(lambda_count):
+            with open(os.path.join(reg_dir, "L%d.py"%i), "w") as f:
+                f.write("def handler(event):\n")
+                f.write("    return %d\n" % i)
+
+        with TestConf(registry=reg_dir):
+            call_each_once_exec(lambda_count=lambda_count)
 
 
 def main():
+    t0 = time.time()
+    
     # general setup
     if os.path.exists(OLDIR):
         try:
@@ -119,10 +172,11 @@ def main():
     run(['./bin/ol', 'new', '-p='+OLDIR])
 
     # run tests with various configs
+    test_reg = os.path.abspath("test-registry")
+    
     startup_pkgs = ["parso", "jedi", "urllib3", "idna", "chardet", "certifi", "requests", "simplejson"]
 
-    with TestConf(launch_worker=False, registry=os.path.abspath("test-registry"),
-                  startup_pkgs=startup_pkgs):
+    with TestConf(launch_worker=False, registry=test_reg, startup_pkgs=startup_pkgs):
         with TestConf(sandbox="sock", handler_cache_size=0, import_cache_size=0, cg_pool_size=10):
             smoke_tests()
         with TestConf(sandbox="sock", handler_cache_size=10000000, import_cache_size=0, cg_pool_size=10):
@@ -136,11 +190,21 @@ def main():
         with TestConf(sandbox="docker", handler_cache_size=10000000, import_cache_size=0, cg_pool_size=0):
             smoke_tests()
 
+    with TestConf(sandbox="sock", handler_cache_size=10000000, import_cache_size=10000000, cg_pool_size=10, registry=test_reg):
+        stress_one_lambda(procs=1, seconds=15)
+        stress_one_lambda(procs=2, seconds=15)
+        stress_one_lambda(procs=8, seconds=15)
+
+    with TestConf(launch_worker=False, sandbox="sock", handler_cache_size=10000000, import_cache_size=10000000, cg_pool_size=10):
+        call_each_once(lambda_count=50)
+        call_each_once(lambda_count=500)
+
     # save test results
     passed = len([t for t in results["runs"] if t["pass"]])
     failed = len([t for t in results["runs"] if not t["pass"]])
     results["passed"] = passed
     results["failed"] = failed
+    results["seconds"] = time.time() - t0
     print("PASSED: %d, FAILED: %d" % (passed, failed))
 
     with open("test.json", "w") as f:
