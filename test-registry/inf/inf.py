a2e4c38450c1849ec7bb95097970de9a56045eb1
wchen329 <wchen329@wisc.edu>
2019-09-07 19:23:21 -0500

    infinite self destruction test handler

---
+++ test-registry/inf/inf.py
@@ -0,0 +1,7 @@
+# Inf:
+# A very simple handler that doesn't really do anything
+# Used to test self destruction mechanism
+def f(event):
+    while(True):
+        tombstone = 0
+    return "Returned but shouldn't, something went wrong?"
