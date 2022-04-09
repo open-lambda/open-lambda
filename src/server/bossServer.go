884a0ed10b7e1d77f07e8a50a9bd83cb40966536
Tyler Caraza-Harter <tylerharter@gmail.com>
2022-04-09 01:51:12 +0000

    make boss handlers thread safe

--- src/server/bossServer.go
+++ src/server/bossServer.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/open-lambda/open-lambda/ol/common"
 )
@@ -14,9 +15,16 @@ const (
 	SCALING_PATH     = "/scaling/worker_count"
 )
 
+type Boss struct {
+	mutex      sync.Mutex
+}
+
 var m = map[string][]int{"workers": []int{}}
 
-func BossStatus(w http.ResponseWriter, r *http.Request) {
+func (b *Boss) BossStatus(w http.ResponseWriter, r *http.Request) {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
 	log.Printf("Receive request to %s\n", r.URL.Path)
 	if b, err := json.MarshalIndent(m, "", "\t"); err != nil {
 		panic(err)
@@ -25,7 +33,10 @@ func BossStatus(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func ScalingWorker(w http.ResponseWriter, r *http.Request) {
+func (b *Boss) ScalingWorker(w http.ResponseWriter, r *http.Request) {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
 	log.Printf("Receive request to %s\n", r.URL.Path)
 	var s []int
 	m["workers"] = append(s, 1)
@@ -40,12 +51,14 @@ func ScalingWorker(w http.ResponseWriter, r *http.Request) {
 }
 
 func BossMain() (err error) {
+	boss := Boss{}
 
 	// things shared by all servers
-	http.HandleFunc(BOSS_STATUS_PATH, BossStatus)
-	http.HandleFunc(SCALING_PATH, ScalingWorker)
+	http.HandleFunc(BOSS_STATUS_PATH, boss.BossStatus)
+	http.HandleFunc(SCALING_PATH, boss.ScalingWorker)
 
 	port := fmt.Sprintf(":%s", common.Conf.Worker_port)
+	fmt.Printf("Listen on port %s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 	panic("ListenAndServe should never return")
 }
