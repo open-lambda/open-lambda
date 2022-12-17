fe61328e02bdd16c6d0a90e2001c3521850e7d78
Kai Mast <kaimast@cs.wisc.edu>
2022-12-16 19:39:41 -0600

    Chore: Update rust toolchain and dependencies (#135)
    * Update rust toolchain and dependencies
    
    * CI: Update rust-cache to v2
    
    * Fix incompatibility with new pandas version
    
    * Re-add W3101 to pylint

--- test-registry/pandas18/f.py
+++ test-registry/pandas18/f.py
@@ -1,4 +1,7 @@
-# ol-install: numpy==1.18.5,pandas
+# ol-install: numpy==1.18.5,pandas==1.4
+
+# newer versions of pandas do not support numpy 1.18 so we are setting pandas to 1.4 here
+
 import numpy
 import pandas
 
