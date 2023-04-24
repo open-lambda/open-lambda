26d85fd37e5be9fe5a7dcd568956193b380c9317
vsagar215 <vsagar@wisc.edu>
2023-04-24 00:33:02 +0000

    ewDOWorkerPool

--- src/boss/do_worker.go
+++ src/boss/do_worker.go
@@ -1,25 +1,80 @@
-func (pool *GcpWorkerPool) CreateInstance(worker *Worker) {
-        // Authenticate
+package boss
 
-        fmt.Printf("Creating Droplet from boss snapshot\n")
+import (
+	"os"
+	"fmt"
+	"time"
+	"context"
+	"strconv"
 
-        // Check if instance already exists
+	"github.com/digitalocean/godo"
+)
 
-        // request body
-	
-	// Make POST: create droplet
+// Globals
+const BOSS_IDX int = 0 // ASSUME: Boss is the first VM created
+const SNAPSHOT_NAME string = "boss snap"
+const CHILD_NAME string = "tony"
 
-	// Set workerIp
-        worker.workerIp = CHILD DROPLET Ip
+type DOWorkerPool struct {
+	Client *godo.Client
+	BossVM godo.Droplet
+	BossKey godo.Key
 }
 
-//this function will only destroy instance from cloud platform
-func (pool *GcpWorkerPool) DeleteInstance(worker *Worker) {
-        // Authenticate
+type DOWorker struct {
+	//TODO: Fill out
+}
 
-        fmt.Printf("Deleting DO worker: %v\n", worker.workerId)
+// Creates new worker pool
+func NewDOWorkerPool() (*WorkerPool) {
+	
+	// Check API Token
+	token := os.Getenv("DIGITALOCEAN_TOKEN")
+	if len(token) == 0 {
+		fmt.Println("ERROR: Unable to find a DigitalOcean personal access token.\n Generate token and export as environment variable named 'DIGITALOCEAN_TOKEN'\n(src: https://docs.digitalocean.com/reference/api/create-personal-access-token/)")
+		panic()
+	}
+	
+	// Establishing auth information
+	client := godo.NewFromToken(token)
+	ctx := context.TODO()
 
-        // Wait until deletion completes
+	// REST call body
+	opt := &godo.ListOptions{
+		Page:    1,
+		PerPage: 200,
+	}
 
-        fmt.Printf("Deleted DO worker %v\n", worker.worker.Id)
+	// Verify SSH key is setup
+	// Make GET: SSH information
+	keys, _, err := client.Keys.List(ctx, opt)
+	if err != nil {
+		fmt.Println("ERROR: An error was encountered while listing SSH information. Aborting...\n")
+		panic(err)
+	} if len(keys) == 0 {
+		fmt.Println("ERROR: Unable to find SSH Key setup.\n Setup SSH keys by visiting https://docs.digitalocean.com/products/droplets/how-to/add-ssh-keys/")
+		panic()
+	}
+	boss_key := keys[BOSS_IDX]
+
+	// Make GET: Boss Info
+	droplets, _, err := client.Droplets.List(ctx, opt)
+	if err != nil {
+		fmt.Println("ERROR: An error was encountered while listing droplets. Aborting...")
+		panic(err)
+	} if len(droplets) == 0 {
+		fmt.Println("ERROR: Unable to find boss. Make sure there is at least ONE VM in active state on your cloud dashboard.\n")
+		panic()
+	}
+	boss_drop := droplets[BOSS_IDX]
+
+	// Fill out DO Worker Pool
+	return &WorkerPool {
+		WorkerPoolPlatform: &DOWorkerPool {
+			Client: client,
+			BossVM: boss_drop,
+			BossKey: boss_key,
+		}
+	}
+}
 }
