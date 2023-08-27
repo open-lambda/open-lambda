deaa1b6a8635c9cc789208a8beee17d8f07607b3
Tyler Caraza-Harter <tylerharter@gmail.com>
2023-08-26 21:06:28 -0500

    vendorize azure dependencies we will need

--- src/vendor/github.com/Azure/azure-sdk-for-go/sdk/azcore/internal/exported/exported.go
+++ src/vendor/github.com/Azure/azure-sdk-for-go/sdk/azcore/internal/exported/exported.go
@@ -7,9 +7,10 @@
 package exported
 
 import (
+	"context"
 	"io"
-	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/internal/shared"
 )
@@ -51,11 +52,32 @@ func Payload(resp *http.Response) ([]byte, error) {
 	if buf, ok := resp.Body.(*shared.NopClosingBytesReader); ok {
 		return buf.Bytes(), nil
 	}
-	bytesBody, err := ioutil.ReadAll(resp.Body)
+	bytesBody, err := io.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		return nil, err
 	}
 	resp.Body = shared.NewNopClosingBytesReader(bytesBody)
 	return bytesBody, nil
+}
+
+// AccessToken represents an Azure service bearer access token with expiry information.
+// Exported as azcore.AccessToken.
+type AccessToken struct {
+	Token     string
+	ExpiresOn time.Time
+}
+
+// TokenRequestOptions contain specific parameter that may be used by credentials types when attempting to get a token.
+// Exported as policy.TokenRequestOptions.
+type TokenRequestOptions struct {
+	// Scopes contains the list of permission scopes required for the token.
+	Scopes []string
+}
+
+// TokenCredential represents a credential capable of providing an OAuth token.
+// Exported as azcore.TokenCredential.
+type TokenCredential interface {
+	// GetToken requests an access token for the specified set of scopes.
+	GetToken(ctx context.Context, options TokenRequestOptions) (AccessToken, error)
 }
