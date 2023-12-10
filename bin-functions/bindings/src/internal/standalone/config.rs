47f09d78fee128d6b013feba0208d064979c8481
Kai Mast <kaimast@cs.wisc.edu>
2023-12-09 20:21:29 -0800

    Fix get_config_value

--- bin-functions/bindings/src/internal/standalone/config.rs
+++ bin-functions/bindings/src/internal/standalone/config.rs
@@ -1,5 +1,3 @@
-use open_lambda_proxy_protocol::CallResult;
-
-pub fn get_config_value(_key: &str) -> CallResult {
+pub fn get_config_value(_key: &str)  -> Result<String, String> {
     unimplemented!();
 }
