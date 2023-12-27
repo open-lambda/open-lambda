60f1f819f828c964d4b36678270475fa55929f72
ShockYoungCHN <yuanzhuoyang@gmail.com>
2023-12-26 20:44:45 -0600

    add cgexec, disable print in server.py for now(stdin/out won't work if cgexec then fork)

--- src/worker/sandbox/sock.go
+++ src/worker/sandbox/sock.go
@@ -72,9 +72,11 @@ func (container *SOCKContainer) freshProc() (err error) {
 
 	var cmd *exec.Cmd
 
-	// todo: make this a Cgroup method. also, why chroot return No such file or directory?
+	cgPath := "/default-ol-sandboxes/" + container.cg.Name()
+	// todo: make this a Cgroup method.
 	if container.rtType == common.RT_PYTHON {
 		cmd = exec.Command(
+			"sudo", "cgexec", "-g", "memory,cpu:"+cgPath,
 			"chroot", container.containerRootDir, "python3", "-u",
 			"/runtimes/python/server.py", "/host/bootstrap.py", strconv.Itoa(1),
 			strconv.FormatBool(common.Conf.Features.Enable_seccomp),
