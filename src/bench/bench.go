0a8871f16f0018eb8e4ca6a78f57504d3b89cfc4
Tyler Caraza-Harter <tylerharter@gmail.com>
2024-01-05 18:03:54 +0000

    add trace playing capability

--- src/bench/bench.go
+++ src/bench/bench.go
@@ -9,6 +9,7 @@ import (
 	"time"
 	"math/rand"
 	"os"
+	"strings"
 	
 	"github.com/urfave/cli/v2"
 
@@ -19,7 +20,7 @@ type Call struct {
 	name string
 }
 
-func task(_ int, reqQ chan Call, errQ chan error) {
+func task(reqQ chan Call, errQ chan error) {
 	for {
 		call, ok := <-reqQ
 		if !ok {
@@ -51,6 +52,114 @@ func task(_ int, reqQ chan Call, errQ chan error) {
 	}
 }
 
+func play_trace_cmd(ctx *cli.Context) (error) {
+	if result, err := play_trace(ctx); err != nil {
+		return err
+	} else {
+		fmt.Printf("%s\n", result)
+		return nil
+	}
+}
+
+func play_trace(ctx *cli.Context) (string, error) {
+	tracePath := ctx.String("trace")
+	if tracePath == "" {
+		return "", fmt.Errorf("'trace' is required, and should be a path to a text file with one lambda function name per line")
+	}
+	traceText, err := ioutil.ReadFile(tracePath)
+	if err != nil {
+		return "", err
+	}
+	traceCalls := strings.Split(string(traceText), "\n")
+
+	tasks := ctx.Int("tasks")
+	if tasks == 0 {
+		tasks = 1
+	}
+
+	seconds := ctx.Float64("seconds")
+	if seconds == 0 {
+		seconds = 60.0
+	}
+
+	olPath, err := common.GetOlPath(ctx)
+	if err != nil {
+		return "", err
+	}
+	configPath := filepath.Join(olPath, "config.json")
+	if err := common.LoadConf(configPath); err != nil {
+		return "", err
+	}
+
+	// launch request threads
+	reqQ := make(chan Call, tasks)
+	errQ := make(chan error, tasks)
+	for i := 0; i < tasks; i++ {
+		go task(reqQ, errQ)
+	}
+
+	// issue requests for specified number of seconds
+	fmt.Printf("start benchmark (%.1f seconds, %d tasks)\n", seconds, tasks)
+	errors := 0
+	successes := 0
+	waiting := 0
+
+	start := time.Now()
+	progressSnapshot := 0.0
+	progressSuccess := 0
+	callIdx := 0
+	for {
+		elapsed := time.Since(start).Seconds()
+		if elapsed >= seconds {
+			break
+		}
+
+		select {
+		case reqQ <- Call{name: traceCalls[callIdx]}:
+			waiting += 1
+			callIdx = (callIdx + 1) % len(traceCalls)
+		case err := <-errQ:
+			if err != nil {
+				errors += 1
+				fmt.Printf("%s\n", err.Error())
+			} else {
+				successes += 1
+				progressSuccess += 1
+			}
+			waiting -= 1
+		}
+
+		// show throughput stats about every 1 seconds
+		if elapsed > progressSnapshot + 1 {
+			fmt.Printf("throughput: %.1f/second\n", float64(progressSuccess) / (elapsed-progressSnapshot))
+			progressSnapshot = elapsed
+			progressSuccess = 0
+		}
+	}
+	seconds = time.Since(start).Seconds()
+
+	// cleanup request threads
+	fmt.Printf("cleanup\n")
+	close(reqQ)
+	waiting += tasks // each needs to send one last nil to indicate it is done
+	for waiting > 0 {
+		if err := <-errQ; err != nil {
+			errors += 1
+			fmt.Printf("%s\n", err.Error())
+		}
+		waiting -= 1
+	}
+
+	if errors > 5*(errors+successes)/100 {
+		panic(fmt.Sprintf(">5%% of requests failed (%d/%d)", errors, errors+successes))
+	}
+
+	result := fmt.Sprintf("{\"benchmark\": \"%s\",\"seconds\": %.3f, \"successes\": %d, \"errors\": %d, \"ops/s\": %.3f}",
+		"play", seconds, successes, errors, float64(successes)/seconds)
+
+	return result, nil
+}
+
 func run_benchmark(ctx *cli.Context, name string, tasks int, functions int, func_template string) (string, error) {
 	num_tasks := ctx.Int("tasks")
 	if num_tasks != 0 {
@@ -77,7 +186,7 @@ func run_benchmark(ctx *cli.Context, name string, tasks int, functions int, func
 	reqQ := make(chan Call, tasks)
 	errQ := make(chan error, tasks)
 	for i := 0; i < tasks; i++ {
-		go task(i, reqQ, errQ)
+		go task(reqQ, errQ)
 	}
 
 	// warmup: call lambda each once
@@ -213,7 +322,7 @@ func BenchCommands() []*cli.Command {
 			Name:      "init",
 			Usage:     "creates lambdas for benchmarking",
 			UsageText: "ol bench init [--path=NAME]",
-            Action: create_lambdas,
+			Action: create_lambdas,
 			Flags: []cli.Flag{
 				&cli.StringFlag{
 					Name:  "path",
@@ -222,6 +331,34 @@ func BenchCommands() []*cli.Command {
 				},
 			},
 			// TODO: add param to decide how many to create
+		},
+		{
+			Name:      "play",
+			Usage:     "play a trace using a .txt file with one lambda function per line",
+			UsageText: "ol bench play --trace=<trace.txt> [--path=NAME]",
+			Action: play_trace_cmd,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "path",
+					Aliases: []string{"p"},
+					Usage: "Path location for OL environment",
+				},
+				&cli.StringFlag{
+					Name:  "trace",
+					Aliases: []string{"f"},
+					Usage: "Path to text file with one lambda name per line",
+				},
+				&cli.Float64Flag{
+					Name:  "seconds",
+					Aliases: []string{"s"},
+					Usage: "Seconds to run (after warmup)",
+				},
+				&cli.IntFlag{
+					Name:  "tasks",
+					Aliases: []string{"t"},
+					Usage: "number of parallel tasks to run (only for parallel bench)",
+				},
+			},
 		},
 	}
 
