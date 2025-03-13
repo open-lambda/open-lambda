c84bfe2226f9284f4f458aea2c4b048f1e324031
Mandukhai Alimaa <mandukhaialimaa@gmail.com>
2025-03-13 01:36:50 -0500

    working on setting up local worker

--- src/boss/cloudvm/local_worker.go
+++ src/boss/cloudvm/local_worker.go
@@ -1,62 +1,95 @@
 package cloudvm
 
 import (
-	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
-	"os"
 	"os/exec"
+	"sync"
 )
 
 type LocalWorkerPool struct {
-	// no platform specific attributes
+	mu      sync.Mutex              // Mutex to protect concurrent access to the map
+	workers map[string]*LocalWorker // Map to store workers by workerId
 }
 
 type LocalWorker struct {
-	Worker
-	workerProcess *exec.Cmd
-	stdinPipe     *os.File      // Pipe for sending tasks to the process
-	stdoutPipe    *bufio.Reader // Pipe for reading output from the process
+	workerId string
+	cmd      *exec.Cmd
+	port     int
 }
 
 func NewLocalWorkerPool() *WorkerPool {
 	return &WorkerPool{
-		WorkerPoolPlatform: &LocalWorkerPool{},
-	}
-}
-
-func (_ *LocalWorkerPool) NewWorker(workerId string) *LocalWorker {
-	return &LocalWorker{
-		Worker: Worker{
-			workerId: workerId,
+		WorkerPoolPlatform: &LocalWorkerPool{
+			workers: make(map[string]*LocalWorker), // Initialize the map
 		},
-		workerProcess: nil, // No process started yet
 	}
 }
 
-func (w *LocalWorker) StartProcess() error {
-	cmd := exec.Command()
-}
-
-func (_ *LocalWorkerPool) CreateInstance(worker *LocalWorker) error {
-	log.Printf("created new local worker: %s\n", worker.workerId)
-
-	if worker.workerProcess == nil {
-		err := worker.StartProcess()
-		if err != nil {
-			return fmt.Errorf("failed to start a worker process: %v", err)
+func getAvailablePort() int {
+	// Example: Start from port 5000 and increment until an available port is found
+	port := 5000
+	for {
+		addr := fmt.Sprintf(":%d", port)
+		listener, err := net.Listen("tcp", addr)
+		if err == nil {
+			listener.Close()
+			return port
 		}
+		port++
+	}
+}
+
+func (pool *LocalWorkerPool) NewWorker(workerId string, command string, args ...string) *LocalWorker {
+	pool.mu.Lock()         // Lock the map for concurrent access
+	defer pool.mu.Unlock() // Unlock when the function returns
+
+	port := getAvailablePort()
+
+	argsWithPort := append(args, fmt.Sprintf("--port=%d", port))
+
+	// TODO: create a worker script that starts a HTTP server and listen to the port
+	cmd := exec.Command(command, argsWithPort...)
+	err := cmd.Start()
+	if err != nil {
+		log.Fatalf("Failed to start worker %s: %v", workerId, err)
 	}
 
-	log.Printf("worker has started successfully", worker.workerId)
-	return nil
+	worker := &LocalWorker{
+		workerId: workerId,
+		cmd:      cmd,
+		port:     port,
+	}
+
+	pool.workers[workerId] = worker // Add the worker to the map
+	fmt.Printf("Worker %s started on port %d\n", workerId, port)
+	return worker
 }
 
-func (_ *LocalWorkerPool) DeleteInstance(worker *LocalWorker) {
-	log.Printf("deleted local worker: %s\n", worker.workerId)
+func (_ *LocalWorkerPool) CreateInstance(worker *LocalWorker) {
+	log.Printf("create new instance of local worker: %s\n", worker.workerId)
 }
 
-func (_ *LocalWorkerPool) ForwardTask(w http.ResponseWriter, _ *http.Request, worker *LocalWorker) {
-	log.Printf("forwarding task")
+func (pool *LocalWorkerPool) DeleteInstance(workerId string) {
+	pool.mu.Lock()         // Lock the map for concurrent access
+	defer pool.mu.Unlock() // Unlock when the function returns
+
+	worker, exists := pool.workers[workerId] // Retrieve the worker from the map
+	if !exists {
+		log.Printf("Worker %s not found", workerId)
+		return
+	}
+
+	log.Printf("Stopping worker: %s\n", workerId)
+	err := worker.cmd.Process.Kill()
+	if err != nil {
+		log.Printf("Failed to kill worker %s: %v", workerId, err)
+	}
+	delete(pool.workers, workerId) // Remove the worker from the map
+}
+
+func (pool *LocalWorkerPool) ForwardTask(w http.ResponseWriter, r *http.Request, worker *LocalWorker) {
+	forwardTaskHelper(w, r, "localhost") // TODO: pass the port.
 }
