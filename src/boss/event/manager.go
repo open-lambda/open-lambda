5828764699966872f5b7b81f7c99de47bda14711
Mandukhai Alimaa <mandukhaialimaa@gmail.com>
2025-05-20 15:15:12 -0500

    kafka trigger

--- src/boss/event/manager.go
+++ src/boss/event/manager.go
@@ -4,13 +4,20 @@ import (
 	"github.com/open-lambda/open-lambda/ol/common"
 )
 
+type Trigger interface {
+	Register(functionName string)
+	Unregister(functionName string)
+}
+
 type Manager struct {
 	cronScheduler *CronScheduler
+	kafkaManager  *KafkaManager
 }
 
 func NewManager() *Manager {
 	return &Manager{
 		cronScheduler: NewCronScheduler(),
+		kafkaManager:  NewKafkaManager(),
 	}
 }
 
@@ -18,8 +25,13 @@ func (m *Manager) Register(functionName string, triggers common.Triggers) {
 	if len(triggers.Cron) > 0 {
 		m.cronScheduler.Register(functionName, triggers.Cron)
 	}
+
+	if len(triggers.Kafka) > 0 {
+		m.kafkaManager.Register(functionName, triggers.Kafka)
+	}
 }
 
 func (m *Manager) Unregister(functionName string) {
 	m.cronScheduler.Unregister(functionName)
+	m.kafkaManager.Unregister(functionName)
 }
