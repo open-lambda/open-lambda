be0243379b6ee7660b86d632f239061f4908c93b
Mandukhai Alimaa <mandukhaialimaa@gmail.com>
2025-06-24 15:46:52 -0500

    fix test

--- bin-functions/install-native.sh
+++ bin-functions/install-native.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+set -e
 shopt -s nullglob
 
 REGISTRY_PATH=$1
@@ -10,11 +11,19 @@ mkdir -p ${REGISTRY_PATH}
 echo "Searching for functions in ${NATIVE_PREFIX}"
 
 for f in ${NATIVE_PREFIX}/*; do
-    name=${f/${NATIVE_PREFIX}/}
+    name=$(basename "$f")
 
     # Ignore subdirectories, libraries, and non-executable files
     if [[ $name != *".so" && -f "$f" && -x "$f" ]]; then
         echo "Installing native function '$name.bin' from '$f' to ${REGISTRY_PATH}/${name}.bin"
-        rsync -c $f ${REGISTRY_PATH}/$name.bin
+        func_name="${name}"
+        archive_name="${func_name}.tar.gz"
+        tmp_dir=$(mktemp -d)
+
+        echo "Packaging native function '${func_name}' → ${archive_name}"
+
+        cp "$f" "${tmp_dir}/f.bin"
+        tar -czf "${REGISTRY_PATH}/${archive_name}" -C "$tmp_dir" f.bin
+        rm -r "$tmp_dir"
     fi
 done
