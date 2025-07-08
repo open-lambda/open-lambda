e90d699005aad4970a9cf592bbe8efa6f765cb92
Mandukhai Alimaa <mandukhaialimaa@gmail.com>
2025-07-07 20:13:56 -0500

    fix test

--- scripts/test.py
+++ scripts/test.py
@@ -393,7 +393,7 @@ def main():
     parser.add_argument('--test_blocklist', type=str, default="")
     parser.add_argument('--registry', type=str, default="")  # Will use worker registry by default
     parser.add_argument('--ol_dir', type=str, default="test-dir")
-    parser.add_argument('--image', type=str, default="ol-min")
+    parser.add_argument('--image', type=str, default="ol-wasm")
     parser.add_argument('--install_examples', action='store_true', 
                        help='Install example functions using admin install command')
 
