bf8990044d5620862f5f5f2fee2208314ff5978d
Jaime Zepeda Pina <jzepedapina@gmail.com>
2025-10-16 19:53:58 +0000

    updated lambda-config-test to reflect WSGI changes

--- examples/lambda-config-test/f.py
+++ examples/lambda-config-test/f.py
@@ -6,8 +6,7 @@ def page_not_found(e):
 app = Flask("hi")
 app.register_error_handler(404, page_not_found)
 
-# TODO: modify wrappers so "/" is the root
-@app.route("/run/lambda-config-test", methods=["GET", "POST", "PUT", "DELETE"])
+@app.route("/", methods=["GET", "POST", "PUT", "DELETE"])
 def hi():
   print("in hi() of lambda-config-test/f.py")
   teapot = 418 # I'm a teapot (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/418)
